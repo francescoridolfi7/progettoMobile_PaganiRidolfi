896c4660191121eb9dcd8bb6a39a3b9977b5a04f
andrepagans <andreapagani26@gmail.com>
2023-11-05 16:09:16 +0100

    Creazione classe per interagire con l'API NBA

---
+++ lib/data/api/nba_api.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NbaApi {
+  final String apiKey;
+  final String apiUrl = 'rapidapi.com';
+
+  NbaApi(this.apiKey);
+
+  Future<Map<String, dynamic>> getNBATeamList() async {
+    final response = await http.get(
+      Uri.parse('$apiUrl/teams'),
+      headers: {
+        'X-RapidAPI-Host' : 'api-nba-v1.p.rapid.com',
+        'X-RapidAPI-Key' : '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
+      },
+    );
+
+    if(response.statusCode == 200){
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception('Errore nella richiesta API');
+    }
+  }
+}
