f1912c76e49536a206c983bf2c4fba52eedd87af
andrepagans <andreapagani26@gmail.com>
2023-11-12 16:23:16 +0100

    Creazione view models per le schermate dell'app e creazione del metodo getNBATeamDetails

--- lib/data/api/nba_api.dart
+++ lib/data/api/nba_api.dart
@@ -11,16 +11,35 @@ class NbaApi {
     final response = await http.get(
       Uri.parse('$apiUrl/teams'),
       headers: {
-        'X-RapidAPI-Host' : 'api-nba-v1.p.rapid.com',
-        'X-RapidAPI-Key' : '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapid.com',
+        'X-RapidAPI-Key': '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
       },
     );
 
-    if(response.statusCode == 200){
+    if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
       return data;
     } else {
       throw Exception('Errore nella richiesta API');
+    }
+  }
+
+  Future<Map<String, dynamic>> getNBATeamDetails(int teamId) async {
+    final response = await http.get(
+      Uri.parse(
+          '$apiUrl/teams/$teamId'), 
+      headers: {
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapid.com',
+        'X-RapidAPI-Key': '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'Errore durante la chiamata API per i dettagli della squadra');
     }
   }
 }
