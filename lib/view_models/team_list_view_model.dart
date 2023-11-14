429be10d82f7c1e4922f7da162178a8efbc578ac
Ridolfi Francesco <francesco.ridolfi7@studio.unibo.it>
2023-11-14 11:43:26 +0100

    Aggiunta metodi per ottenere dati locali precedentemente salvati e aggiornamento dati

--- lib/view_models/team_list_view_model.dart
+++ lib/view_models/team_list_view_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/api/nba_api.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/models/nba_team.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_application_progettomobile_pagani_ridolfi/local_storage/nba_data_storage.dart';
 
 class TeamListViewModel extends ChangeNotifier {
   Future<List<NbaTeam>> getTeams(BuildContext context) async {
@@ -11,4 +12,14 @@ class TeamListViewModel extends ChangeNotifier {
         .map((teamData) => NbaTeam.fromJson(teamData))
         .toList();
   }
+
+  Future<void> saveTeamsLocally(List<NbaTeam> teams) async {
+    final localStorageService = LocalStorageService();
+    await localStorageService.saveNbaTeamData(teams);
+  }
+}
+
+Future<List<NbaTeam>> loadTeamsLocally() async {
+  final localStorageService = LocalStorageService();
+  return await localStorageService.getNbaTeamData();
 }
