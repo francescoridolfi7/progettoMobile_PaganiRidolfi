6b4e55026f7beec05022521359c5448d4973002f
andrepagans <andreapagani26@gmail.com>
2023-12-05 17:14:19 +0100

    fix

--- lib/ui/screens/team_details_screen.dart
+++ lib/ui/screens/team_details_screen.dart
@@ -4,14 +4,13 @@ import 'package:flutter_application_progettomobile_pagani_ridolfi/data/models/nb
 class TeamDetailsScreen extends StatelessWidget {
   final NbaTeam team;
 
-  const TeamDetailsScreen({super.key, required this.team});
+  const TeamDetailsScreen({Key? key, required this.team}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dettagli Squadra',
-            style: TextStyle(color: Colors.white)),
+        title: const Text('Dettagli Squadra', style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromARGB(255, 29, 66, 138),
       ),
       body: Padding(
@@ -24,7 +23,14 @@ class TeamDetailsScreen extends StatelessWidget {
             Text('Città: ${team.city}'),
             Text('Conferenza: ${team.leagues.standard.conference}'),
             Text('Divisione: ${team.leagues.standard.division}'),
-            const SizedBox(height: 16), // Aggiunto const qui
+            const SizedBox(height: 16),
+            // Aggiunta della visualizzazione del logo
+            Image.network(
+              team.logo,
+              width: 100, // Puoi personalizzare le dimensioni del logo secondo le tue esigenze
+              height: 100,
+              fit: BoxFit.contain, // Per mantenere le proporzioni del logo
+            ),
           ],
         ),
       ),
