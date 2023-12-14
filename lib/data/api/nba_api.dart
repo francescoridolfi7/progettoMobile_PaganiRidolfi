faaf2eac8ab965a9fd26f75d94425fb604605e7b
andrepagans <andreapagani26@gmail.com>
2023-12-14 09:21:13 +0100

    fix

--- lib/data/api/nba_api.dart
+++ lib/data/api/nba_api.dart
@@ -1,24 +1,14 @@
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
-import 'package:dio/dio.dart';
-import 'package:logger/logger.dart';
 
 class NbaApi {
   String apiKey = '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb';
   final String apiUrl = 'https://api-nba-v1.p.rapidapi.com';
-  final _logger = Logger();
 
   NbaApi(this.apiKey);
 
   Future<Map<String, dynamic>> getNBATeamList() async {
-    final cachedData = await _getLocalCache('teamList');
-    if (cachedData != null) {
-      return cachedData;
-    }
-
     final response = await http.get(
       Uri.parse('$apiUrl/teams'),
       headers: {
@@ -29,7 +19,6 @@ class NbaApi {
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      _saveLocalCache('teamList', data);
       return data;
     } else {
       throw Exception('Errore nella richiesta API');
@@ -37,11 +26,6 @@ class NbaApi {
   }
 
   Future<Map<String, dynamic>> getNBAStandings(int season) async {
-    final cachedData = await _getLocalCache('standings_$season');
-    if (cachedData != null) {
-      return cachedData;
-    }
-
     final Map<String, String> headers = {
       'X-RapidAPI-Key': '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
@@ -59,7 +43,6 @@ class NbaApi {
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      _saveLocalCache('standings_$season', data);
       return data;
     } else {
       throw Exception(
@@ -68,11 +51,6 @@ class NbaApi {
   }
 
   Future<Map<String, dynamic>> getNBAGames(String date) async {
-    final cachedData = await _getLocalCache('nbagames');
-    if (cachedData != null) {
-      return cachedData;
-    }
-
     final Map<String, String> headers = {
       'X-RapidAPI-Key': '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
@@ -88,7 +66,6 @@ class NbaApi {
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      _saveLocalCache('games', data);
       return data;
     } else {
       throw Exception(
@@ -96,7 +73,8 @@ class NbaApi {
     }
   }
 
-  Future<Map<String, dynamic>> getNBARoster(int teamId, int selectedSeason) async {
+  Future<Map<String, dynamic>> getNBARoster(
+      int teamId, int selectedSeason) async {
     final Map<String, String> headers = {
       'X-RapidAPI-Key': '4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb',
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
@@ -107,15 +85,13 @@ class NbaApi {
       'season': selectedSeason.toString(),
     };
 
-    final Uri uri = Uri.parse('$apiUrl/players')
-        .replace(queryParameters: params);
-
+    final Uri uri =
+        Uri.parse('$apiUrl/players').replace(queryParameters: params);
 
     final http.Response response = await http.get(uri, headers: headers);
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      _saveLocalCache('roster_${selectedSeason}_$teamId', data);
       return data;
     } else {
       throw Exception(
@@ -138,92 +114,15 @@ class NbaApi {
     final Uri uri =
         Uri.parse('$apiUrl/teams/statistics').replace(queryParameters: params);
 
-
     final http.Response response = await http.get(uri, headers: headers);
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
-      _saveLocalCache(
-          'statistics_2020', data); 
       return data;
     } else {
       throw Exception(
         'Errore nella richiesta HTTP. Codice di stato: ${response.statusCode}',
       );
-    }
-  }
-
-  static Future<Map<String, dynamic>> _fetchAndSaveData(
-      Map<String, dynamic> args) async {
-    final url = args['url'] as String;
-    final cacheKey = args['cacheKey'] as String;
-    final headers = args['headers'] as Map<String, String>;
-
-    final cachedData = await _getLocalCache(cacheKey);
-    if (cachedData != null) {
-      return cachedData;
-    }
-
-    final response = await Dio().get(url, options: Options(headers: headers));
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.data);
-      _saveLocalCache(cacheKey, data);
-      return data;
-    } else {
-      throw Exception('Errore nella richiesta API');
-    }
-  }
-
-  static Future<void> _saveLocalCache(
-      String key, Map<String, dynamic> data) async {
-    final prefs = await SharedPreferences.getInstance();
-    final currentTime = DateTime.now().millisecondsSinceEpoch;
-
-    final dataWithTimestamp = {
-      'timestamp': currentTime,
-      'data': data,
-    };
-
-    prefs.setString(key, jsonEncode(dataWithTimestamp));
-  }
-
-  static Future<Map<String, dynamic>?> _getLocalCache(String key) async {
-    final prefs = await SharedPreferences.getInstance();
-    final cachedData = prefs.getString(key);
-
-    if (cachedData != null) {
-      final Map<String, dynamic> decodedData = jsonDecode(cachedData);
-
-      final timestamp = decodedData['timestamp'] ?? 0;
-      final currentTime = DateTime.now().millisecondsSinceEpoch;
-      const maxAge = 60 * 60 * 1000; // Dati validi per 1 ora
-
-      if (currentTime - timestamp <= maxAge) {
-        return decodedData['data'];
-      } else {
-        prefs.remove(key);
-        return null;
-      }
-    } else {
-      return null;
-    }
-  }
-
-  Future<void> fetchDataAndSaveInBackground() async {
-    final args = {
-      'url': '$apiUrl/teams',
-      'cacheKey': 'teamList',
-      'headers': {
-        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
-        'X-RapidAPI-Key': apiKey,
-      },
-    };
-
-    try {
-      await _fetchAndSaveData(args);
-    } catch (e) {
-      _logger.e('Errore durante il recuper ed il salvataggio dei dati:$e');
     }
   }
 }
--- lib/view_models/team_list_view_model.dart
+++ lib/view_models/team_list_view_model.dart
@@ -3,7 +3,6 @@ import 'package:logger/logger.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/api/nba_api.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/models/nba_team.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_application_progettomobile_pagani_ridolfi/local_storage/nba_data_storage.dart';
 
 class TeamListViewModel extends ChangeNotifier {
   final Logger _logger = Logger();
@@ -29,18 +28,9 @@ class TeamListViewModel extends ChangeNotifier {
 
       return teams;
     } catch (e, stacktrace) {
-      _logger.e('Errore durante il recupero delle squadre', error: e, stackTrace: stacktrace);
+      _logger.e('Errore durante il recupero delle squadre',
+          error: e, stackTrace: stacktrace);
       return [];
     }
-  }
-
-  Future<void> saveTeamsLocally(List<NbaTeam> teams) async {
-    final localStorageService = LocalStorageService();
-    await localStorageService.saveNbaTeamData(teams);
-  }
-
-  Future<List<NbaTeam>> loadTeamsLocally() async {
-    final localStorageService = LocalStorageService();
-    return await localStorageService.getNbaTeamData();
   }
 }
