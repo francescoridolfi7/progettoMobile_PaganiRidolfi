d76b0dd066de1179040cd34b7e3e0c11e4048a0c
andrepagans <andreapagani26@gmail.com>
2023-12-19 12:18:00 +0100

    Eseguito il recupero delle squadre in background

--- lib/view_models/team_list_view_model.dart
+++ lib/view_models/team_list_view_model.dart
@@ -1,11 +1,11 @@
-import 'dart:convert'; 
-import 'dart:io'; 
+import 'dart:convert';
+import 'dart:io';
+import 'dart:isolate';  // Aggiunto per utilizzare gli isolati
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/api/nba_api.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/models/nba_team.dart';
-import 'package:provider/provider.dart';
 
 class TeamListViewModel extends ChangeNotifier {
   final Logger _logger = Logger();
@@ -40,34 +40,61 @@ class TeamListViewModel extends ChangeNotifier {
     }
   }
 
+  // Funzione per eseguire la chiamata API in background
+  Future<void> _fetchTeamsInBackground(SendPort sendPort) async {
+    try {
+      _logger.i('Inizio richiesta API in background');
+
+      final nbaApi = NbaApi('4315828859msh068310ee9c40e90p1b5d6fjsn973d9e4b1fbb');
+      final teamList = await nbaApi.getNBATeamList();
+
+      final teamDataList = teamList['response'] as List<dynamic>?;
+
+      if (teamDataList == null) {
+        _logger.w('Attenzione: La lista delle squadre è null!');
+        sendPort.send([]);
+        return;
+      }
+
+      final teams =
+          teamDataList.map((teamData) => NbaTeam.fromJson(teamData)).toList();
+
+      if (teams.isEmpty) {
+        _logger.w('Attenzione: La lista delle squadre è vuota!');
+      }
+
+      _logger.i('Fine richiesta API in background');
+
+      sendPort.send(teams);
+    } catch (e, stacktrace) {
+      _logger.e('Errore durante il recupero delle squadre',
+          error: e, stackTrace: stacktrace);
+      sendPort.send([]);
+    }
+  }
 
   Future<List<NbaTeam>> getTeams(BuildContext context) async {
     try {
+      _logger.i('Inizio recupero delle squadre');
+
       final cachedTeams = await _readTeamsFromFile();
 
       if (cachedTeams.isNotEmpty) {
+        _logger.i('Squadre lette dalla cache');
         return cachedTeams;
       } else {
-        // ignore: use_build_context_synchronously
-        final nbaApi = Provider.of<NbaApi>(context, listen: false);
-        final teamList = await nbaApi.getNBATeamList();
+        final receivePort = ReceivePort();
 
-        final teamDataList = teamList['response'] as List<dynamic>?;
+        // Avvia un nuovo isolato per eseguire la chiamata API in background
+        await Isolate.spawn(_fetchTeamsInBackground, receivePort.sendPort);
 
-        if (teamDataList == null) {
-          _logger.w('Attenzione: La lista delle squadre è null!');
-          return [];
+        final List<NbaTeam> teams = await receivePort.first;
+
+        if (teams.isNotEmpty) {
+          await _writeTeamsToFile(teams);
         }
 
-        final teams =
-            teamDataList.map((teamData) => NbaTeam.fromJson(teamData)).toList();
-
-        if (teams.isEmpty) {
-          _logger.w('Attenzione: La lista delle squadre è vuota!');
-        }
-
-        
-        await _writeTeamsToFile(teams);
+        _logger.i('Fine recupero delle squadre');
 
         return teams;
       }
