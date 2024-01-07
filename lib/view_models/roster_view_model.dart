f69bb1478575f73d699bb203a8d2b08db6fc1fdd
andrepagans <andreapagani26@gmail.com>
2024-01-07 14:16:57 +0100

    fix

--- lib/view_models/roster_view_model.dart
+++ lib/view_models/roster_view_model.dart
@@ -5,7 +5,6 @@ import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/models/nba_roster.dart';
 import 'package:flutter_application_progettomobile_pagani_ridolfi/data/api/nba_api.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class RosterViewModel extends ChangeNotifier {
   final NbaApi nbaApi;
@@ -46,20 +45,11 @@ class RosterViewModel extends ChangeNotifier {
 
   Future<List<NbaPlayer>> getRoster(int teamId, int selectedSeason) async {
     try {
-      const lastUpdateTimeKey = 'lastUpdateTimeRoster';  // Use a unique key for roster
+      final cachedRoster = await _readRosterFromFile(teamId, selectedSeason);
 
-      final prefs = await SharedPreferences.getInstance();
-      final lastUpdateTime = prefs.getInt(lastUpdateTimeKey) ?? 0;
-      final currentTime = DateTime.now().millisecondsSinceEpoch;
-
-      if (currentTime - lastUpdateTime > 24 * 60 * 60 * 1000) {
-        // E' passato più di 24 ore dall'ultima chiamata API
-        final cachedRoster = await _readRosterFromFile(teamId, selectedSeason);
-
-        if (cachedRoster.isNotEmpty) {
-          return cachedRoster;
-        }
-
+      if (cachedRoster.isNotEmpty) {
+        return cachedRoster;
+      } else {
         final Map<String, dynamic> rosterData =
             await nbaApi.getNBARoster(teamId, selectedSeason);
         final List<dynamic> playerDataList = rosterData['response'] ?? [];
@@ -67,21 +57,11 @@ class RosterViewModel extends ChangeNotifier {
             .map((playerData) => NbaPlayer.fromJson(playerData))
             .toList();
 
+        
         await _writeRosterToFile(teamId, selectedSeason, roster);
 
-        // Aggiorna il timestamp dell'ultima chiamata API
-        await prefs.setInt(lastUpdateTimeKey, currentTime);
-
         return roster;
-      } else {
-        final cachedRoster = await _readRosterFromFile(teamId, selectedSeason);
-
-        if (cachedRoster.isNotEmpty) {
-          return cachedRoster;
-        }
       }
-
-      return [];
     } catch (e, stacktrace) {
       _logger.e('Errore durante il recupero del roster', error: e, stackTrace: stacktrace);
       return [];
